2d6219f98408eff3def5001fc436a589ca11bac0
Victor Mendes <victorutdsim7@gmail.com>
2022-10-05 16:17:25 -0300

    add loading img while fetching

--- src/components/LoadingForm.js
+++ src/components/LoadingForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './loadingForm.module.scss';
+import loadingIcon from '../images/loading-icon.gif';
 
 class LoadingForm extends Component {
   render() {
@@ -7,8 +8,12 @@ class LoadingForm extends Component {
       <div className={ styles.loading }>
         <div data-testid="fetching" className={ styles.loading_form }>
           Loading...
+          <img src={ loadingIcon } alt="loading gif" />
         </div>
-        <div className={ styles.loading_button }>Loading...</div>
+        <div className={ styles.loading_button }>
+          Loading...
+          <img src={ loadingIcon } alt="loading gif" />
+        </div>
       </div>
     );
   }
